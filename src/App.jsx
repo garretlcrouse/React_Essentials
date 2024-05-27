ecf6f331e203211d0ba29a36b83b6c049c14d753
Garret Crouse <garretlcrouse@icloud.com>
2024-05-27 10:44:14 -0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Header from './components/Header.jsx';
 import { CORE_CONCEPTS } from './data.js';
 import CoreConcept from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  function handleSelect(selectedButton) {
+
+  }
+
+
+
   return (
     <div>
       <Header />
@@ -18,6 +25,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
